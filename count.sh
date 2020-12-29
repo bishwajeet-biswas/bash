b0598e3e790790367fabf75e6329ee604ba8e851
gitvishvjeet <vishvjeet@outlook.com>
2020-12-29 22:31:37 +0530

    command to get the required output

--- count.sh
+++ count.sh
@@ -1,14 +1,1 @@
-#/bin/bash!
-# first lets get the uniq content from the file
-
- cat logfile | awk '{print $1}' | uniq > uniqfiles
-
-echo "this script is going to get the ip addresses printed in first column and their word counts"
-
-for i in `cat uniqfiles`
-do
-    echo -n " $i " 
-    grep $i logfile | wc -l 
-done
-
-rm uniqfiles
+awk '{print $1}' logfile | sort | uniq -c | sort -nr | head -n 8 | awk '{print $2, $1}'
